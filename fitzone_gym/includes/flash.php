ed743334c57e231b0022ad3a75f0ba03e988b3a4
Philopateer waheed <filowaheed28@gmail.com>
2025-12-14 15:10:26 +0200

    Delete fitzone_gym directory

--- fitzone_gym/includes/flash.php
+++
@@ -1,8 +0,0 @@
-<?php
-// includes/flash.php
-$flash = getFlash();
-if($flash): ?>
-    <div style="background: <?php echo $flash['type'] == 'success' ? '#2ecc71' : '#e74c3c'; ?>; padding: 10px; border-radius: 5px; margin-bottom: 20px; color: white;">
-        <?php echo $flash['message']; ?>
-    </div>
-<?php endif; ?>
